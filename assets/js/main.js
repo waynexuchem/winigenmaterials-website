f871ae41e3abc9310e54aa769b0e0fcfb7fb1b94
Wayne Xu <48770588+waynexuchem@users.noreply.github.com>
2026-05-04 15:10:07 -0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+const toggle=document.querySelector('.mobile-toggle');const menu=document.querySelector('.mobile-menu');if(toggle&&menu){toggle.addEventListener('click',()=>menu.classList.toggle('open'));}
