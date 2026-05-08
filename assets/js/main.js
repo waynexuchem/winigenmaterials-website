253f087841c1c351cfae238fdfab3fe378271314
Wayne Xu <48770588+waynexuchem@users.noreply.github.com>
2026-05-07 21:25:53 -0400

    Delete assets/js/main.js

--- assets/js/main.js
+++
@@ -1,1 +0,0 @@
-const toggle=document.querySelector('.mobile-toggle');const menu=document.querySelector('.mobile-menu');if(toggle&&menu){toggle.addEventListener('click',()=>menu.classList.toggle('open'));}
